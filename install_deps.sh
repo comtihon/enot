11ffe7cbd1d9f1b2dbbbef6f7a2c3cb8deaf3245
Valerii Tikhonov <com.tihon@mail.ru>
2017-05-15 23:03:27 +0200

    fix install pash

--- install_deps.sh
+++ install_deps.sh
@@ -4,7 +4,7 @@ if [ ! -f  "$HOME/artifactory/artifactory" ]; then
   wget https://bintray.com/jfrog/artifactory/download_file?file_path=jfrog-artifactory-oss-5.3.0.zip \
        -O $HOME/artifactory/artifactory.zip
   unzip $HOME/artifactory/artifactory.zip
-  mv $HOME/artifactory-oss-* $HOME/artifactory/artifactory
+  mv $HOME/artifactory/artifactory-oss-* $HOME/artifactory/artifactory
 else
   echo "Using cached artifactory."
 fi

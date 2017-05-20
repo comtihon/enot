6135dfbeced2df0934476f9d21d36fae33899434
Val <valerii.tikhonov@klarna.com>
2017-05-20 15:56:55 +0200

    fix script

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ ! -f  "$HOME/artifactory/artifactory" ]; then
+if [ ! -d  "$HOME/artifactory/artifactory" ]; then
   mkdir -p $HOME/artifactory
   wget https://bintray.com/jfrog/artifactory/download_file?file_path=jfrog-artifactory-oss-5.3.0.zip \
        -O $HOME/artifactory/artifactory.zip

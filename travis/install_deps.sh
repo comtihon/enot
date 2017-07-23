95dcc73fc5c6d28502408fbeeffbb81a1cec0cb3
Valerii Tikhonov <com.tihon@mail.ru>
2017-07-23 13:26:25 +0200

    fix install deps

--- travis/install_deps.sh
+++ travis/install_deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ ! -d  "$HOME/artifactory/artifactory" ]; then
   mkdir -p $HOME/artifactory
-  artifactorywget https://bintray.com/jfrog/artifactory/download_file?file_path=jfrog-artifactory-oss-5.3.0.zip \
+  wget https://bintray.com/jfrog/artifactory/download_file?file_path=jfrog-artifactory-oss-5.3.0.zip \
        -O $HOME/artifactory/artifactory.zip
   unzip $HOME/artifactory/artifactory.zip -d $HOME/artifactory
   mv $HOME/artifactory/artifactory-oss-* $HOME/artifactory/artifactory
